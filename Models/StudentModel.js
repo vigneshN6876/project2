cee484b2bbfd8a324a8e318a9659e071ceea171f
vigneshN6876 <nagarajvicky07@gmail.com>
2025-07-02 22:09:11 +0530

    third commit

--- Models/StudentModel.js
+++ Models/StudentModel.js
@@ -1,13 +1,15 @@
-const express = requires('express')
+const express = require('express')
 const mongoose = require('mongoose')
 
 const Student = mongoose.Schema({
     Name: {
-        "type" : "String",
-        "required" : true
+        type : String,
+        required : true
     },
     rollno: {
-        "type" : "String",
-        "required" : true
+        type : String,
+        required : true
     }
 })
+
+module.exports =  Student 
--- Server.js
+++ Server.js
@@ -1,16 +1,39 @@
 const express = require('express')
 const Port = 5025;
 const app = express();
-
+const Student = require('../project2/Models/StudentModel')
+const mongoose = require('mongoose')
+app.use(express.json())
 app.listen(Port , () => {
     console.log("Server listening to port " , Port);
 })
-
-app.get('/api/students' , (req , res) => {
-    res.send("Hello world")
+mongoose.connect('mongodb://localhost:27017/studentsdb')
+.then(() => {
+    console.log("DB connected");
 })
-app.post('/api/students' , () => {
-    
+.catch((err) => {
+    console.log(err);
+})
+app.get('/api/students' , async (req , res) => {
+    try {
+        const studetails = await Student.find();
+        if(!studetails){
+                res.status(400).json({message : "no details found"});
+        }
+        res.send(studetails)
+    } catch (error) {
+        res.status(400).json({message : "no details found"});
+    }
+})
+app.post('/api/students' , async (req , res) => {
+    try {
+        const {Name , rollno } = req.body;
+        const studentdetails = new Student({Name , rollno});
+            studentdetails.save()
+        res.send(studentdetails)
+    } catch (error) {
+        res.status(400).json({message : error.message});
+    }
 })
 app.put('/api/students' , () => {
     
