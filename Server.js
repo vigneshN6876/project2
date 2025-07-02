625a54c3af827191eb2561b35ab637c10b27e123
vigneshN6876 <nagarajvicky07@gmail.com>
2025-07-02 21:32:41 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const Port = 5025;
+
+const app = express();
+
+app.listen(Port , () => {
+    console.log("Server listening to port " , Port);
+})
+
+app.get('/api/students' , (req , res) => {
+    res.send("Hello world")
+})
+app.post('/api/students' , () => {
+    
+})
+app.put('/api/students' , () => {
+    
+})
+app.delete('/api/students' , () => {
+    
+})
